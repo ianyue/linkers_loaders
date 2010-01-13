1b5d99b1dbb485c19248681db5afb360b7808fc6
Jianping Yu <ian.j.yue@gmail.com>
2010-01-13 16:00:45 +0800

    support multiple input files for readObj function.

--- linker.py
+++ linker.py
@@ -2,38 +2,56 @@
 
 import allocator
 
-# segments table information
-segmentTable = {}
+# object information
+objectTable = {}
 
-# symbol table
-symbolTable = {}
 
-# relocation information
-relocation = {}
+class Object(object) :
+    'An object file information.'
+    # file or archive name, if any
+    name = ''
+    # # of segments
+    nseg = 0
+    # # of symbols
+    nsym = 0
+    # # of relocs
+    nrel = 0
+    # hash names to segment numbers (below)
+    segnames = {}
+    # array of segments, index starts with 1
+    segs = ['']
+    # hash names to symbol numbers
+    symnames = {}
+    # array of symbols (below), index starts with 1
+    syms = ['']
+    # array of relocs (below)
+    rels = []
 
-# segment data
-segmentData = {}
-
-# code segment data
-#textData = ''
-
-# data segment data
-#dataData = ''
-
-# bss segment data
-#bssData = ''
+    def __init__(self) :
+        self.name = ''
+        self.nseg = 0
+        self.nsym = 0
+        self.nrel = 0
+        self.segnames = {}
+        self.segs = ['']
+        self.symnames = {}
+        self.syms = ['']
+        self.rels = []
 
 class SegmentInfo() :
     'Segment Infomation'
     readable = False
     writable = False
     present = False
+    #data = ''
     
-    def __init__(self, name, addr, length, flags) :
+    def __init__(self, name, segNo, base, length, flags) :
         self.name = name
-        self.addr = addr
-        self.length = length
+        self.segNo = segNo
+        self.base = int(base, 16)
+        self.length = int(length, 16)
         self.setFlags(flags)
+        self.data = ''
 
     def setFlags(self, flags) :
         if 'R' in flags :
@@ -48,9 +66,10 @@ class Symbol() :
     'Symbol table entity'
     defined = False
 
-    def __init__(self, name, value, seg, type) :
+    def __init__(self, name, symNo, value, seg, type) :
         self.name = name
-        self.value = value
+        self.symNo = symNo
+        self.value = int(value, 16)
         self.seg = seg
         self.setType(type)
 
@@ -65,7 +84,7 @@ class Relocation() :
     'relocation entity'
 
     def __init__(self, loc, seg, ref, type) :
-        self.loc = loc
+        self.loc = int(loc, 16)
         self.seg = seg
         self.ref = ref
         self.type = type
@@ -78,47 +97,58 @@ class SymbolReloc() :
         self.ref = ref
 
 
-def readFile (fileName) :
-    'read infomation from file.'
-    global segmentTable, symbolTable, relocation
+def readObj (fileName) :
+    'read object infomation from file.'
+    global objectTable
     fp = open(fileName)
     magicNumber = fp.readline()
     print magicNumber
 
+    o = Object()
+
+    o.name = fileName
+
+    print o.name
+    
+    objectTable[o.name] = o
     # read number of segment, number of symbol table entries,
     #+ number of relocation entities.
     s = fp.readline()
     args = s.split(' ')
-    nsegs = args[0]
-    nsyms = args[1]
-    nrels = args[2]
-
+    o.nseg = int(args[0], 16)
+    o.nsym = int(args[1], 16)
+    o.nrel = int(args[2], 16)
+    
     # read segsments
     
-    for i in range(1, int(nsegs, 16) + 1) :
+    for i in range(1, o.nseg + 1) :
         s = fp.readline()
         s = s.strip()
         members = s.split(' ')
-        info = SegmentInfo(members[0], members[1], members[2], members[3])
-        segmentTable[i] = info
+        info = SegmentInfo(members[0], i, members[1], members[2], members[3])
+        
+        o.segnames[info.name] = i
+        o.segs.append(info)
 
     
     # read symbols
-    for i in range(1, int(nsyms, 16) + 1) :
+    for i in range(1, o.nsym + 1) :
         s = fp.readline()
         s = s.strip()
         members = s.split(' ')
-        info = Symbol(members[0], members[1], members[2], members[3])
-        symbolTable[i] = info
+        info = Symbol(members[0], i, members[1], members[2], members[3])
+        
+        o.symnames[info.name] = i
+        o.syms.append(info)
 
 
     # read rels
-    for i in range(1, int(nrels, 16) + 1) :
+    for i in range(0, o.nrel) :
         s = fp.readline()
         s = s.strip()
         members = s.split(' ')
         info = Relocation(members[0], members[1], members[2], members[3])
-        relocation[i] = info
+        o.rels.append(info)
 
 
     # read data
@@ -127,40 +157,27 @@ def readFile (fileName) :
         data += line.strip() + ' '
 
     data = data.strip()
-    readData(segmentTable, data)
+    readData(o, data)
 
     fp.close()
 
-def readData(segmentTable, data) :
+def readData(obj, data) :
     'read segment data'
-    global segmentData
     
-    for key in segmentTable:
-        info = segmentTable[key]
-        count = int(info.length, 16)
+    for i in range(1, obj.nseg + 1) :
+        info = obj.segs[i]
+        count = info.length
         if count == 0:
             continue
-
-        if info.name not in segmentData :
-            segmentData[info.name] = ''
             
-        segmentData[info.name] += data[:(3 * count - 1)] + ' '
+        info.data += data[:(3 * count - 1)] + ' '
         data = data[3 * count:]
-        #if info.name == '.text' :
-        #    textData += data[:3 * count - 1] + ' '
-        #    data = data[3 * count:]
-        #elif info.name == '.data' :
-        #    dataData += data[:3 * count - 1] + ' '
-        #    data = data[3 * count:]
-        #elif info.name == '.bss' :
-        #    bssData += data[:3 * count - 1] + ' '
-        #    data = data[3 * count:]
 
-    for key in segmentData :
-        segmentData[key] = segmentData[key].strip()
-    #textData = textData.strip()
-    #dataData = dataData.strip()
-    #bssData = bssData.strip()
+    #for key in segmentData :
+    #    segmentData[key] = segmentData[key].strip()
+    for i in range(1, obj.nseg + 1) :
+        obj.segs[i].data = obj.segs[i].data.strip()
+
 
 # write object file routines
 def my_cmp(s1, s2) :
@@ -173,7 +190,7 @@ def writeFile_3_1(fileName) :
 
     for key in segmentTable:
         info = segmentTable[key]
-        fp.write(info.name + ' ' + info.addr + ' ' + info.length + 
+        fp.write(info.name + ' ' + info.base + ' ' + info.length + 
                  ' ' + str(info.readable) + ' ' + str(info.writable) +
                  ' ' + str(info.present) + '\n')
 
@@ -255,7 +272,10 @@ def writeFile_4_3(fileName) :
 
 if __name__ == '__main__' :
 
-    readFile('example.txt')
-    writeFile_4_2('output.txt')
-    for key in segmentData :
-        print key, segmentData[key]
+    readObj('example.txt')
+    readObj('example1.txt')
+    #writeFile_4_2('output.txt')
+    for key in objectTable :
+        obj = objectTable[key]
+        for name in obj.segnames :
+            print key, name, obj.segs[obj.segnames[name]].data
